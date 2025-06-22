1de78b855cc34d2125db9a813f67700b1ff612e6
rangjin <rangjin2037@gmail.com>
2025-06-23 00:02:15 +0900

    Model Interface 추가

--- src/domain/user/repository/user.repository.ts
+++ src/domain/user/repository/user.repository.ts
@@ -1,12 +1,12 @@
-import { User } from '../model/user.model';
+import { IUser, User } from '../model/user.model';
 
 class UserRepository {
 
-    async save(email: string, password: string) {
+    async save(email: string, password: string): Promise<IUser> {
         return await new User({ email, password }).save();
     }
 
-    async findByEmail(email: String) {
+    async findByEmailorNull(email: String): Promise<IUser | null> {
         return await User.findOne({ email });
     }
 
--- src/domain/word/models/word.model.ts
+++ src/domain/word/models/word.model.ts
@@ -1,10 +1,21 @@
-import { Schema, model } from 'mongoose';
+import { Schema, Document, model } from 'mongoose';
 
-const wordSchema = new Schema({
+export type JLPTLevel = 'JLPT1' | 'JLPT2' | 'JLPT3' | 'JLPT4' | 'JLPT5';
+
+export interface IWord extends Document {
+    word: string;
+    reading: string;
+    meaning: string;
+    level: JLPTLevel;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const wordSchema = new Schema<IWord>({
   word: { type: String, required: true },
   reading: { type: String, required: true },
   meaning: { type: String, required: true },
   level: { type: String, enum: ['JLPT1', 'JLPT2', 'JLPT3', 'JLPT4', 'JLPT5'], required: true }
 }, { timestamps: true });
 
-export const Word = model('Word', wordSchema);
+export const Word = model<IWord>('Word', wordSchema);
