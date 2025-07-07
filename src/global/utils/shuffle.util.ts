76c780ab786b7fff0c6d676b9db618cdea506c54
rangjin <rangjin2037@gmail.com>
2025-07-08 06:46:49 +0900

    주석 삭제

--- src/global/utils/shuffle.util.ts
+++ src/global/utils/shuffle.util.ts
@@ -1,5 +1,5 @@
 export function shuffleArray<T>(array: T[]): T[] {
-    const result = [...array]; // 원본을 수정하지 않도록 복사
+    const result = [...array];
     for (let i = result.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [result[i], result[j]] = [result[j], result[i]];
